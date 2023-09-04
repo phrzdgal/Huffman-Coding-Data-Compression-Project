b64d374888fa428467ac23c2d6a437ef90ef780c
Phiroze Duggal <97709147+phrzdgal@users.noreply.github.com>
2023-09-04 01:57:19 -0700

    Update compress.cpp

--- compress.cpp
+++ compress.cpp
@@ -17,9 +17,13 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < inputFile.filesize(); i++) {
             aMap[i] = 0;
     }
-    while ((int byteValue = inputFile.read_byte()) != EOF) {
-        aMap[byteValue] = aMap[byteValue]++;
+
+    int z = 0;
+    while ((int byteValue = inputFile.read_byte()) != -1) {
+        aMap[z] = aMap[byteValue];
+        z++;
     }
+    
     for (int i = 0; i < inputFile.filesize(); i++) {
             freqVector.push_back(aMap[i]);
     }
